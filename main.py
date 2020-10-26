0841eec61f689d9601661a3f5aa43f4008f30b59
Sherif Abdou <sherif_abdou@outlook.com>
2020-10-25 19:04:33 -0700

    Test For Deployment

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import spacy
 import sys
 from spacy import displacy
+from spacy.tokens import Token
 
 text = ""
+other_nlp = spacy.load("./advanced_vocab_model")
 with open("input.txt") as f:
     text = f.read()
 
-nlp = spacy.load("en_core_web_lg")
-doc = nlp(text)
+for ent in other_nlp(text).ents:
+    print(ent.text)
 
-def get_subjs(text):
-    arr =  [v.text for v in text.noun_chunks if v.root.dep_ in {"nsubj", "nsubjpass"} and not v.root.is_stop]
-    arr += [v.text for v in text if v.tag_ == "VBG" and v.dep_ in {"nsubj", "nsubjpass"}]
-    return arr
+def is_advanced(token):
+    other_tok = next(iter(other_nlp(token.text)))
+    if other_tok.ent_type_ == "ADVANCED_WORD" and other_tok.pos_ == "ADJ":
+        return True
+    return False
 
-# Finds the subjects in the first sentence
+nlp = spacy.load("en_core_web_md")
+Token.set_extension("is_advanced", getter=is_advanced)
+
+def main(text, count):
+    doc = nlp(text)
+    subj = first_subj(doc)[0]
+
+    # count = int(input("How many sentences? "))
+
+    ranked = [v[0].text for v in rank_sentences(doc, nlp(subj), count)]
+    print()
+    string = " ".join(ranked) 
+    print(string.replace("\n", ""))
+    print()
+    return string.replace("\n", "")
+
+
+# Finds the subjects in a text
 def subjs(text):
     def translate(token, sent):
         for chunk in sent.noun_chunks:
@@ -28,6 +48,22 @@ def subjs(text):
 
     return arr
 
+# Finds the objects in a text
+def objs(text):
+    def translate(token, sent):
+        for chunk in sent.noun_chunks:
+            if token == chunk.root:
+                return chunk.text
+        return token.text
+
+    arr = [v for v in text if v.dep_ in {"dobj", "pobj"} and not v.is_stop]
+
+    arr = [translate(v, text) for v in arr]
+
+    return arr
+   
+
+# Finds the subject of the first sentence
 def first_subj(text):
     def translate(token, sent):
         for chunk in sent.noun_chunks:
@@ -39,22 +75,21 @@ def first_subj(text):
 
     return subjs(sent)
 
+
+# Ranks sentences based on similarity to the subject and returns the top i results
+# Returned in order as they appear in the text
 def rank_sentences(text, subj, i):
     items = []
-    f = True
     for j, raw in enumerate(text.sents):
         s = nlp(" ".join(subjs(raw)))
-        items.append((raw, subj.similarity(s), j))
+        o = nlp(" ".join(objs(raw)))
+        score = subj.similarity(s)+(subj.similarity(o)/2)
+        items.append((raw, score, j))
 
     return sorted((sorted(items, key=lambda x: x[1], reverse=True)[:i]), key=lambda x: x[2])
 
+
 if __name__ == "__main__":
-    subj = first_subj(doc)[0]
-
-    count = 3
-
-    ranked = [v[0].text for v in rank_sentences(doc, nlp(subj), count)]
-    print()
-    print(" ".join(ranked))
-    print()
-
+    file_text = text
+    count = input("Count: ")
+    main(file_text, int(count))
---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+import base64
+import main
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def get_data():
+    image = request.form["image"].encode("utf-8")
+    IMAGE = "image"
+    with open("image.png", "wb") as fh:
+        fh.write(base64.decodebytes(image))
+    
+    stringed_image = pytesseract.image_to_string("image.png")
+    print(stringed_image)
+    return stringed_image
+
+@app.route("/summary", methods=["POST"])
+def get_summary():
+    sents = request.form["count"]
+    string = request.form["content"]
+
+    res = main.main(string, int(sents))
+
+    return res
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000, debug=True)
